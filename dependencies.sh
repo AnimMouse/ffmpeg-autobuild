2c7095072557515677c6d8d6689fe9f860d59faf
AnimMouse <git@animmouse.eu.org>
2020-07-04 20:57:09 +0800

    Fix meson

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,3 @@
 sudo apt-get update
-sudo apt-get install ragel g++ cvs yasm automake libtool autoconf gcc make zlib1g-dev pax nasm gperf autogen bzip2 autoconf-archive p7zip-full meson python3-distutils
+sudo apt-get install ragel cvs yasm pax nasm gperf autogen autoconf-archive
+pip3 install meson
