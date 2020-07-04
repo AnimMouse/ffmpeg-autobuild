5ee187894ac32beafb493560437317116b263e57
AnimMouse <git@animmouse.eu.org>
2020-07-04 20:37:21 +0800

    Add dependencies

---
+++ dependencies.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+sudo apt-get install ragel g++ cvs yasm automake libtool autoconf gcc make zlib1g-dev pax nasm gperf autogen bzip2 autoconf-archive p7zip-full meson python3-distutils
