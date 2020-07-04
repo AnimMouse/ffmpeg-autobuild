161c305930e1fd9ad9ed54f1982b6b59fbd10c33
AnimMouse <git@animmouse.eu.org>
2020-07-04 15:03:49 +0800

    Remove gnutls

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
 sudo apt-get install mingw-w64 yasm
-sudo apt-get install libx264-dev libx265-dev libnuma-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev libunistring-dev
+sudo apt-get install libx264-dev libx265-dev libnuma-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev
