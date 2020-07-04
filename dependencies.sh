bc7ce57ef897599564eb8d2fff5350e94bc9a517
AnimMouse <git@animmouse.eu.org>
2020-07-04 16:09:51 +0800

    Remove gnutls

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
 sudo apt-get install autoconf automake mingw-w64 yasm
-sudo apt-get install libass-dev libfreetype6-dev libgnutls28-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev libx264-dev libx265-dev libnuma-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev zlib1g-dev libunistring-dev
+sudo apt-get install libass-dev libfreetype6-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev libx264-dev libx265-dev libnuma-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev zlib1g-dev
