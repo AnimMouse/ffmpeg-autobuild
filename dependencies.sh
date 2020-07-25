8477bb0985c03ab144b4c14a8463cd81a12787f4
AnimMouse <git@animmouse.eu.org>
2020-07-25 23:12:05 +0800

    Add sudo H flag on pip3

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,4 @@
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install ragel cvs yasm pax nasm gperf autogen autoconf-archive python3-setuptools
-sudo pip3 install meson ninja
+sudo -H pip3 install meson ninja
