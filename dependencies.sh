c74df596668631550cdc8375b2e6495db6a5eb9f
AnimMouse <git@animmouse.eu.org>
2021-04-04 20:05:28 +0800

    Remove python3-setuptools as it is already in GH Actions

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install ragel cvs yasm pax nasm gperf autogen autoconf-archive python3-setuptools
+sudo apt-get install ragel cvs yasm pax nasm gperf autogen autoconf-archive
 sudo -H pip3 install meson ninja
