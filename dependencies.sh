b51510db16a64ee417b8e4ddda1518534c12a894
AnimMouse <git@animmouse.com>
2022-10-12 14:21:05 +0800

    Quiet APT, use gh, use ubuntu-latest, and use GITHUB_OUTPUT

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install ragel cvs yasm pax nasm gperf autogen autoconf-archive
-sudo -H pip3 install meson ninja
+sudo apt-get -qq update
+sudo apt-get -qq upgrade
+sudo apt-get -qq install ragel cvs yasm pax nasm gperf autogen autoconf-archive
+sudo -H pip3 -qq install meson ninja
